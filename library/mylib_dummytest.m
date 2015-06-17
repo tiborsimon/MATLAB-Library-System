a4c27726495f8a4ac277322d88f961d28f263fdc
Tibor Simon <tibor@tibor-simon.com>
2015-06-17 15:42:15 +0200

    Update mylib_dummytest.m

--- library/mylib_dummytest.m
+++ library/mylib_dummytest.m
@@ -2,8 +2,6 @@ function [] = mylib_dummytest()
 %MYLIB_DUMMYTEST Function to test the installation of the library.
 %   This function does nothing just prints a message to the output window.
 
-[name, version] = core_getlibrarydata();
-disp(['Dummy test:: ',name, ' ', version, ' has been set up correctly :)']);
+disp(['It is working :)']);
 
 end
-
