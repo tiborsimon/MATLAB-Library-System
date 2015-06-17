b0b9aa24430212cdb608aeacf8a091d5b8b772e1
Tibor Simon <tibor@tibor-simon.com>
2015-06-17 15:55:39 +0200

    Update dummy_lib_test.m

--- library/dummy_lib_test.m
+++ library/dummy_lib_test.m
@@ -1,5 +1,5 @@
-function [] = mylib_dummytest()
-%MYLIB_DUMMYTEST Function to test the installation of the library.
+function [] = dummy_lib_test()
+%DUMMY_LIB_TEST Function to test the installation of the library.
 %   This function does nothing just prints a message to the output window.
 
 disp(['It is working :)']);
